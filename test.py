eb1a329e225dc029313f7bd2177057e8a6fd3154
rishi-v2124 <49381981+rishi-v2124@users.noreply.github.com>
2020-11-03 15:12:17 -0800

    Update and rename test_rishi.py to test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import calcs
+
+distance = 40  # Distance is in miles
+biking_time = 4000  # Time is in seconds
+walking_time = 10000
+car_time = 4500
+
+calcs.calc_car_emissions(distance)
+print()
+calcs.calc_bus_emissions(distance)
+print()
+calcs.calc_motorcycle_emissions(distance)
+print()
+
+calcs.output_vehicle_costs()
