e187a60bb372acaab9b638b52d60cd54c1fe7542
MelvinE1999 <73800751+MelvinE1999@users.noreply.github.com>
2020-11-12 19:47:05 -0800

    Update routes.py
    -Added a Survey class that will work with the survey.html to create the survey page
    -Added a survey route to add the route for the user to see the survey

--- main/webdev/routes.py
+++ main/webdev/routes.py
@@ -1,5 +1,19 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, SubmitField, TextField, SelectField
+
+
+#This program and class will just provide the question variety and the question
+#that the user will have to enter
+class Survey(FlaskForm):
+    q1 = PasswordField('What is your address?')
+    choice = [(1,'Freshman'), (2,'Sophmore'), (3,'Junior'), (4,'Senior')]
+    q2 = SelectField('What Year are you at Sac State:', coerce=int, choices=choice)
+    q3 = TextField('What type of commute option do you use to get to school:\n')
+    q4 = BooleanField('If you live on campus check the box \n')
+    q5 = BooleanField('Check this box if you know about the commuter sleeve\n')
+    submit = SubmitField("submit")
 
 @app.route("/")
 @app.route("/home")
@@ -25,3 +39,18 @@ def biking():
 @app.route("/driving", methods=['GET', 'POST'])
 def driving():
     return render_template('driving.html', title='Driving')
+
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    form = Survey()
+    if form.is_submitted():
+        result = request.form
+        file = open("data.txt", "w+")
+        #this will print the dat to a file
+        #the file only keeps the most recent user's data for now
+        #y means yes and n means no on the file
+        #1-4 on the file corelates to the year they are fresman-Senior
+        for key,value in result.items():
+            file.write("answer to  " + str(key) + "= " + str(value) + "\n" )
+        file.close()
+    return  render_template('survey.html', form=form)
